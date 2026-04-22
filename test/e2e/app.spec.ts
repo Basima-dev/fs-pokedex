5c2f8a71109cfab6c13be34c4975b1ee21e7b412
basima <obasiprincechi@yahoo.com>
2026-04-23 00:51:13 +0100

    added a test

--- test/e2e/app.spec.ts
+++ test/e2e/app.spec.ts
@@ -5,5 +5,13 @@ describe('Pokedex', () => {
      await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  });
+
+  test('navigate from front page to Ivysaur page', async ({ page }) => {
+      await page.goto('/');
+      await page.click('text=ivysaur');
+      await expect(page).toHaveURL(/.*ivysaur/);
+      await expect(page.locator('body')).toContainText('chlorophyll');
   })
+
 })
